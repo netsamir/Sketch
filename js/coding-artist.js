9b99d03143343d2a9f0600c46968de383404adba
Samir Sadek <netsamir@gmail.com>
2017-03-19 13:32:39 +0100

    end of style binding

--- js/coding-artist.js
+++ js/coding-artist.js
@@ -2,12 +2,23 @@ var app = new Vue({
   el: '#app',
   data: {
     message: 'Hello Oana mon amoureuse',
-    clicks: 0
+    clicks: 0,
+    styleSpot1: {
+      background: "white"
+    },
+    controlId: 'red'
   }, // end of data
   methods: {
     MOana: function(){
       this.message = 'I love you'
       this.clicks++;
     }, // end of oana
+    colorMe: function(value){
+      if(value % 2 === 0){
+        this.styleSpot1.background = "red"
+      } else {
+        this.styleSpot1.background = "blue"
+      }
+    } // end of colorMe
   } // end of method
 }) //end of Vue
