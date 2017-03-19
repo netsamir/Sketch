03ba39f32acc05114aff469143f9f943ff498402
Samir Sadek <netsamir@gmail.com>
2017-03-19 19:45:02 +0100

    usage of model

--- js/coding-artist.js
+++ js/coding-artist.js
@@ -3,6 +3,8 @@ var app = new Vue({
   data: {
     message: 'Hello Oana mon amoureuse',
     inputText: '',
+    key: 'vue',
+    result: 'Try it',
     clicks: 0,
     styleSpot1: {
       background: "white"
@@ -11,9 +13,17 @@ var app = new Vue({
   }, // end of data
   methods: {
     MOana: function(){
-      this.message = 'I love you'
+      this.message = 'I love you';
       this.clicks++;
     }, // end of oana
+    updateMessage: function(){
+      if (this.inputText === this.key){
+        this.result = "You Win"
+      }
+      else {
+        this.result = "You loose"
+      }
+    }, // end of updateMessage
     colorMe: function(value){
       if(value % 2 === 0){
         this.styleSpot1.background = "red"
